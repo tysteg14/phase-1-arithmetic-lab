4f6d3c3de548bf1b57c501c12dbe7b226d07d798
Tyrell <tysteg14@gmail.com>
2023-08-07 14:47:18 -0500

    review arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2 
+
+const random = 2
+
+let num3 = 12
+let num4 = 3
+
+const mod = num3 / num4
+
+let numbers = (2, 3, 20)
+const max = Math.max(numbers)
